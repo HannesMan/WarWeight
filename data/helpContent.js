5b0d539e92940bf53c9d4258706c39a1ec040168
HannesMan <61539928+HannesMan@users.noreply.github.com>
2025-06-06 16:30:12 +0300

    Update helpContent.js

--- data/helpContent.js
+++ data/helpContent.js
@@ -6,9 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
       <p>This application helps you prioritize Builder Upgrades to increase War Weight as quickly as possible.</p>
       <ul>
         <li>First, select your Town Hall level from the dropdown menu.</li>
-        <li>Check the boxes for upgrades you have completed.</li>
+        <li>Check the boxes for upgrades you have completed. If you have done all the upgrades, you are ready to move up to the next Town Hall.</li>
         <li>Click the "Calculate Next Upgrades" button to see the upcoming upgrades.</li>
-        <li>You can toggle between dark and light themes using the button at the top right.</li>
+        <li>You can toggle between dark and light themes using the button "Toggle Dark/Light.</li>
+        <li>Feel free to send feedback using the button "Feedback"!.</li>
       </ul>
       <p style="margin-top: 1em; font-style: italic; font-size: 0.9em; color: #666;">
         Made by Hannes Man | BLITZKRIEG FWA
